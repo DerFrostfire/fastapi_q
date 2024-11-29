32a38d849c50e3ec8c2eed79ddc3eeb985bda7f5
Смирнов Дмитрий <skorcher431@gmail.com>
2024-11-29 12:03:43 +0300

    Объединение изменений из IT-321: рефакторинг сохранения вложений

--- backend/src/attachment/utils.py
+++ backend/src/attachment/utils.py
@@ -5,10 +5,12 @@ import aiofiles
 from src.logging.setting import loggings
 
 logger = loggings(__name__)
-async def save_upload_file(upload_files: List[UploadFile], paths: List[str]) -> None:
+async def save_all_upload_files(upload_files: List[UploadFile], paths: List[str]) -> None:
     for file, path in zip(upload_files, paths):
-        logger.info(f'Записываем файл: {path}')
-        async with aiofiles.open(path, 'wb') as out_file:
-            while content := await file.read(1024*1024):  # читаем файл порциями по мегабайту
-                await out_file.write(content)
+        await save_file(file, path)
 
+async def save_file(file, path):
+    logger.info(f'Записываем файл: {path}')
+    async with aiofiles.open(path, 'wb') as out_file:
+        while content := await file.read(1024*1024):
+            await out_file.write(content)
